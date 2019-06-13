d37a6b74a2354511985f26231c2df152abfa2f90
Jet <elesebatanksley@yahoo.com>
2019-06-13 15:52:40 -0500

    created the mvc and startd working on handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO burgers (burger_name) VALUES ('Double patty bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('Incredible burger');
+INSERT INTO burgers (burger_name) VALUES ('Breakfast Burger');
+
